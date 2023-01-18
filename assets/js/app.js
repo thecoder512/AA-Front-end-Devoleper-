c97763d7753f2e195f1a2d0306733e2169df5a93
Ahmed Ayob abdo ali Ayob <108896341+thecoder512@users.noreply.github.com>
2023-01-18 20:32:09 +0200

    create index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,18 @@
 //declating elements
-
 const toggle = document.querySelector(".toggle"),
   toggleContainerOne = document.querySelector("#container"),
   toggleTwo = document.querySelector(".toggle-two"),
   navMenu = document.querySelector(".nav-menu"),
   traingle = document.querySelectorAll(".traingle"),
-  navLink = document.querySelectorAll(".list-link");
+  navLink = document.querySelectorAll(".list-link"),
+  main = document.getElementById('main');
 
 //show the menu and second toggle and remove the first toggle
 toggle.addEventListener("click", () => {
   toggle.classList.add("toggle-out");
   toggleContainerOne.classList.toggle("tra-in");
   navMenu.classList.add("nav-animation");
+  main.classList.add('main-animation')
 });
 
 //remove the menu and second toggle and add the first toggle
@@ -22,6 +23,7 @@ toggleTwo.addEventListener("click", () => {
   toggle.classList.remove("toggle-out");
   toggleContainerOne.classList.remove("tra-in");
   navMenu.classList.remove("nav-animation");
+  main.classList.remove('main-animation')
   // this for removing the blur color after the animation to start new onw with white color
   setTimeout(() => {
     traingle.forEach((e) => {
@@ -52,9 +54,13 @@ navLink.forEach((e) => {
 const letter = document.querySelectorAll('.letter');
 letter.forEach((e) => {
   e.addEventListener('mouseover', () => {
-    e.classList.add('animation')
+    // in keyframes you will find animation keyframe if you want to know how this animation work
+    e.classList.add('animate__animated')
+    e.classList.add('animate__rubberBand')
+    
     setTimeout(() => {
-      e.classList.remove('animation')
-    }, 500);
+      e.classList.remove('animate__animated')
+      e.classList.remove('animate__rubberBand')
+    }, 800);
   });
 })
