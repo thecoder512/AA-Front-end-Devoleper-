c6830ed6ebeed32349ccdf9bc7b4fca802118e94
Ahmed Ayob abdo ali Ayob <108896341+thecoder512@users.noreply.github.com>
2023-01-20 16:48:10 +0200

    index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,3 +64,30 @@ letter.forEach((e) => {
     }, 800);
   });
 })
+
+
+// loading animation
+
+const logoShape = document.querySelector('.logo-shape')
+const loading = document.querySelector('.loading')
+const homeTitle = document.querySelector('.home-title')
+const homeSubtitle = document.querySelector('.home-subtitle')
+
+window.onload = () => {
+  logoShape.classList.add('animation')
+  main.classList.add('tags')
+  setTimeout(() => {
+    loading.classList.add('logo-after-animtion')
+  }, 2000);
+  setTimeout(() => {
+    toggle.classList.add('toggle-on-loading')
+  }, 3000);
+  setTimeout(() => {
+    homeTitle.classList.add('home-title-animation')
+  }, 3500);
+  setTimeout(() => {
+    homeSubtitle.classList.add('home-title-animation')
+  }, 4000);
+  
+}
+  
